c47cc4da0c4715f3c4352d466604493ab0766ed2
ahmadd201433.githup.io <ahmadd201433@gmail.com>
2022-12-08 20:51:52 +0300

    edit

--- css/search.js
+++ css/search.js
@@ -1,24 +1,15 @@
-// get all element 
-const searchBar = document.querySelector('.textsearch')
-const elements = document.querySelectorAll('#data-items li')
-
-
-
-// make search bar interactive with keyboard
-searchBar.addEventListener('keyup', (e) => {
-
-    // get value of 
-    let seachVal = e.target.value
-
-    // extract all Li 
-    elements.forEach((elm) => {
-        // make all Li hidden
-        elm.style.display = "none"
-        // check if Li have the same value search bar
-        elm.outerText.includes(seachVal) ?
-            elm.style.display = "" 
-            :elm.style.display = "none"
-
-    })
-
-})
+    // JavaScript code
+    function search_animal() {
+        let input = document.getElementById('searchbar').value
+        input=input.toLowerCase();
+        let x = document.getElementsByClassName('card');
+        
+        for (i = 0; i < x.length; i++) {
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                x[i].style.display="none";
+            }
+            else {
+                x[i].style.display="list-item";		
+            }
+        }
+    }
